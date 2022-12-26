be794376baaddc3aa66dc8ef91a8175a19a83976
huyphamfc <huyphamfc@gmail.com>
2022-12-26 11:21:42 +0700

    create ReminderList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+
 import './App.css';
+import Reminder from 'models/reminder';
+import { ReminderList } from 'components';
+
+const reminders: Reminder[] = [
+  { id: 1, title: 'Reminder 1' },
+  { id: 2, title: 'Reminder 2' },
+];
 
 function App() {
-  return <button className="btn btn-primary">Click Me</button>;
+  return <ReminderList items={reminders} />;
 }
 
 export default App;
